3a01ce9741c31b24e87a24bf59ceb8ef382ea8cd
Jennifer Perez <strawberries2254@gmail.com>
2026-03-02 16:14:23 -0500

    still working on loop quiz

--- js/loops.js
+++ js/loops.js
@@ -1,4 +1,6 @@
 let loop_quiz;
+let print_quiz;
+let curr;
 
 function getJSON()
 {
@@ -6,21 +8,20 @@ function getJSON()
     function(data)
     {
         loop_quiz = data["loop-quiz"];
-        console.log(loop_quiz);
         showQuiz();
     })
 }
 
 function showQuiz()
 {
-    const print_quiz = document.getElementById("exercises");
-    let curr = loop_quiz[0];
+    print_quiz = document.getElementById("exercises");
+    curr = loop_quiz[0];
     let quiz_div = 
         `<div class="quiz-box">
         <h2>Exercises</h2>
         <h3>${curr.question}</h3>
         <pre>${curr.code}</pre>
-        div class="options">
+        <div class="options">
         `;
     for (let i = 0; i < curr.options.length; i++) {
         quiz_div += 
@@ -33,11 +34,34 @@ function showQuiz()
     }
     quiz_div += 
         `
-            </div>
-            <button onclick="checkAns()">Check Answer</button>
-            </div>
+        </div>
+        <button onclick="checkAns()">Check Answer</button>
+        </div>
         `;
     print_quiz.innerHTML = quiz_div;
+}
+
+function checkAns()
+{
+    let check = document.querySelector('input[name="quiz-ans"]:checked');
+    let which;
+    if (!check) {
+        alert("Please check an answer");
+    } else if (document.getElementById('0').checked) {
+        which = 0;
+    } else if (document.getElementById('1').checked) {
+        which = 1;
+    } else if (document.getElementById('2').checked) {
+        which = 2;
+    } else if (document.getElementById('3').checked) {
+        which = 3;
+    }
+
+    if (curr.correct === which) {
+        alert(`${curr.feedback}`);
+    } else if (curr.correct != which && check) {
+        alert("That's incorrect, try again!");
+    }
 }
 
 getJSON();
